9bf35a56643c945b8fd58d363f5d7fb7a4a7c1a6
Gustavo Kenzo Araki Takechi <gustavokenzo314@gmail.com>
2021-09-16 10:59:00 -0300

    sunburst + table and chart

--- application.py
+++ application.py
@@ -80,7 +80,7 @@ comum = sorted(set(publicadoras_filtro), key=publicadoras_filtro.count, reverse=
 # Dados 2
 
 generos = []
-todos_generos= ['Sports', 'Racing', 'Role-Playing', 'Puzzle', 'Misc', 'Shooter', 'Simulation', 'Action']
+todos_generos= ['Esportes', 'Corrida', 'RPG', 'Puzzle', 'Diversos', 'Tiro', 'Simulação', 'Ação']
 # Gêneros mais populares:
 # Sports, Racing, Role-Playing, Puzzle, Misc, Shooter, Simulation, Action
 
@@ -93,6 +93,7 @@ fps = 0
 sim = 0
 action = 0
 
+# Para cada jogo, pegar o número de vendas globais (índice 10 no csv) e ir somando de acordo com o gênero do jogo
 for linha in df_array:
     if linha[4] == 'Sports':
         sports += int(linha[10])
@@ -111,6 +112,7 @@ for linha in df_array:
     if linha[4] == 'Action':
         action += int(linha[10])
 
+# Guardar o número de vendas numa array para usar no gráfico de linhas 
 generos.append(sports)
 generos.append(racing)
 generos.append(rpg)
@@ -120,6 +122,9 @@ generos.append(fps)
 generos.append(sim)
 generos.append(action)
 
+# Criação da tabela
+# A primeira linha indica o nome de cada coluna
+# As linhas seguintes associam o nome do gênero ao número de vendas
 tabela = [['Gênero', 'Vendas'],
             ['Esportes', sports],
             ['Corrida', racing],
@@ -133,15 +138,16 @@ tabela = [['Gênero', 'Vendas'],
 # ----------------------------------------------------------------------------------
 
 # Dados 5
-# n deu certo ainda
 plataforma = []
 
+# Guardar a plataforma de cada jogo numa array chamada plataforma
 for linha in df_array:
     try:
         plataforma.append(linha[2].upper())
     except:
         continue
-    
+
+# Contar quantos jogos foram publicados para cada console
 ps3 = plataforma.count('PS3')
 computador = plataforma.count('PC')
 ps4 = plataforma.count('PS4')
@@ -156,15 +162,23 @@ atari = plataforma.count('2600')
 wii = plataforma.count('WII')
 ng = plataforma.count('NG')
 
+# Fazer as associações como se fosse uma regra da cadeia
+# Plataforma contém todas as empresas
+# Cada empresa engloba seus consoles
 snk = ng
 nintendo = nes + n64 + gb + wii
 microsoft = x360 + xone 
 sony = ps3 + ps4 + ps2 + psp
 plataforma = sony + microsoft + computador + nintendo + atari + snk
 
+# Dict = dicionário
+# Em consoles, colocar primeiro as empresas e depois os consoles
+# Em empresas, colocar "plataforma" nas mesmas posições em que existe uma empresa na lista acima e,
+# em seguida, colocar o nome da empresa na mesma posição de cada console dessa empresa
+# Em vendas, colocar o valor de vendas, que vai determinar o tamanho de cada setor do gráfico
 data = dict (
-    empresas = ['plataforma', 'Sony', 'Microsoft', 'PC', 'Nintendo', 'Atari', 'SNK', 'PlayStation3', 'Computador', 'PlayStation4', 'NES', 'PlayStation2', 'Xbox360', 'Nintendo64', 'XboxOne', 'PlayStationPortable', 'GameBoy', 'Atari2600', 'NintendoWii' , 'NeoGeo'],
-    consoles = ['', 'plataforma', 'plataforma','plataforma','plataforma','plataforma','plataforma', 'Sony', 'PC', 'Sony', 'Nintendo', 'Sony', 'Microsoft', 'Nintendo', 'Microsoft', 'Sony', 'Nintendo', 'Atari', 'Nintendo', 'SNK' ],
+    consoles = ['Plataforma', 'Sony', 'Microsoft', 'PC', 'Nintendo', 'Atari', 'SNK', 'PlayStation 3', 'Computador', 'PlayStation4', 'NES', 'PlayStation 2', 'Xbox 360', 'Nintendo 64', 'Xbox One', 'PSP', 'GameBoy', 'Atari2600', 'Nintendo Wii' , 'NeoGeo'],
+    empresas = ['', 'Plataforma', 'Plataforma','Plataforma','Plataforma','Plataforma','Plataforma', 'Sony', 'PC', 'Sony', 'Nintendo', 'Sony', 'Microsoft', 'Nintendo', 'Microsoft', 'Sony', 'Nintendo', 'Atari', 'Nintendo', 'SNK' ],
     vendas = [plataforma, sony, microsoft, computador, nintendo, atari, snk, ps3, computador, ps4, nes, ps2, x360, n64, xone, psp, gb, atari, wii, ng]) 
 
 
@@ -179,14 +193,21 @@ PLOTLY E DASH
 '''
 
 
-# Quando for testar no Dash, selecionar tudo entre as aspas triplas e apertar Alt + Shift + A para des-comentar
+# Quando for testar com o Dash, selecionar tudo entre as aspas triplas e apertar Alt + Shift + A para des-comentar
 # Inicializar o Dash na variável app
 app = dash.Dash(__name__)
+
+# Criar os gráficos
+
+# ========================================== Gráfico 1 =============================================
+# make_subplots foi importado lá em cima
 fig1 = make_subplots(
     rows=1, cols=1,
     shared_xaxes=True,
     vertical_spacing=0.03,
     specs=[[{"type": "scatter"}]]
+
+# Adicionar o traço, ou, a linha do gráfico de fato
 )
 fig1.add_trace(
     go.Scatter(
@@ -198,6 +219,8 @@ fig1.add_trace(
     row=1, col=1
 )
 
+# ========================================== Gráfico 2 =============================================
+# ff foi importado
 fig2 = ff.create_table(tabela, height_constant=50)
 
 trace_fig2 = go.Scatter(x=todos_generos, y=generos, xaxis='x2', yaxis='y2',
@@ -211,7 +234,7 @@ fig2['layout']['yaxis2'] = {}
 fig2.layout.xaxis.update({'domain': [0, .5]})
 fig2.layout.xaxis2.update({'domain': [.6, 1]})
 fig2.layout.yaxis2.update({'anchor': 'x2'})
-fig2.layout.yaxis2.update({'title': 'Vendas'})
+fig2.layout.yaxis2.update({'title': 'Vendas (em milhões de unidades)'})
 fig2.layout.margin.update({'t':75, 'l':50, 'r': 70})
 
 fig2.update_layout(
@@ -224,10 +247,11 @@ fig2.update_layout(
     )
 )
 
+# ========================================== Gráfico 5 =============================================
 fig5 = px.sunburst(
     data,
-    names='empresas',
-    parents='consoles',
+    names='consoles',
+    parents='empresas',
     values='vendas',
     color='empresas', hover_data=['empresas'],
     color_continuous_scale='Inferno',
@@ -235,11 +259,14 @@ fig5 = px.sunburst(
 )
 fig5.layout.update({'height':800})
 
-# Estilizar o Dash
-app.layout = html.Div([
-    html.H1("Teste"),
+# ==================================================================================================
 
-    html.Br(),
+# Estilizar o Dash
+# Layout do Dash, sempre que quiser fazer o gráfico aparecer, colocar aqui
+app.layout = html.Div([
+    html.H1("Grupo 4 - Vendas de Jogos"), # H1 = Heading 1, ou cabeçalho
+
+    html.Br(), # Br =  break, ou quebra de linha, para deixar mais espaçado
     dcc.Graph(figure = fig1),
 
     html.Br(),
@@ -248,6 +275,9 @@ app.layout = html.Div([
     html.Br(),
     dcc.Graph(figure = fig5)
 ])
+
 # Rodar o Dash
+# Para ficar mais dinâmico, basta deixar o código rodando apertar Ctrl + S para salvar,
+# O Dash vai atualizar sozinho a cada 5 segundos +-, ou vc pode só clicar em reload mesmo
 if __name__ == "__main__":
     app.run_server(debug=True)
